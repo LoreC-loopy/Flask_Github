86a6225c0cc1a16d797564481eaf593b6e860859
LoreC-loopy <mcpebearpunch@gmail.com>
2025-11-15 00:02:51 -0800

    login display upon open

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import os
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+	return render_template('login.html')
+
 @app.route('/register')
 def register():
 	return render_template('register.html')
